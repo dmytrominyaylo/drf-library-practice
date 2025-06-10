ba0895407e7c431b58adb5a490cfb1d5f3350ef7
Dmytro Minyaylo <dimon9660@gmail.com>
2025-06-10 21:01:53 +0300

    define Payment views for processing and tracking

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
 
-# Create your views here.
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.select_related("borrowing")
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.request.user.is_staff:
+            return queryset
+        return queryset.filter(borrowing__user=self.request.user)
