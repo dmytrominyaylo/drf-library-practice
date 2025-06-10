6585701fe557f50940c77e86a13b5340470196d9
Dmytro Minyaylo <dimon9660@gmail.com>
2025-06-10 21:04:14 +0300

    setup URLs for Payment-related views

---
+++ payments/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+from django.urls import path, include
+from payments.views import PaymentViewSet
+
+app_name = "payments"
+
+router = routers.DefaultRouter()
+router.register("payments", PaymentViewSet, basename="payments")
+
+urlpatterns = [path("", include(router.urls))]
