54cccf5a32d211ee2d5ef809d115bf358ca1c62f
Dmytro Minyaylo <dimon9660@gmail.com>
2025-06-10 20:53:30 +0300

    implement Payment model and generate migration file

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from borrowings.models import Borrowing
 
-# Create your models here.
+
+class Payment(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PENDING", "Pending"
+        PAID = "PAID", "Paid"
+
+    class TypeField(models.TextChoices):
+        PAYMENT = "PAYMENT", "Payment"
+        FINE = "FINE", "Fine"
+
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
+    type_field = models.CharField(max_length=20, choices=TypeField.choices)
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
+    session_url = models.URLField()
+    session_id = models.CharField(max_length=255)
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.borrowing.user}, {self.type_field} - {self.status} - {self.money_to_pay}"
