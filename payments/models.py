1d8cf169037fd13a100fa692567f03aad7cd22f3
Dmytro Minyaylo <dimon9660@gmail.com>
2025-06-11 22:51:36 +0300

    update Payment model: added session_url and session_id fields, created migrations

--- payments/models.py
+++ payments/models.py
@@ -14,8 +14,8 @@ class Payment(models.Model):
     status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
     type_field = models.CharField(max_length=20, choices=TypeField.choices)
     borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
-    session_url = models.URLField()
-    session_id = models.CharField(max_length=255)
+    session_url = models.URLField(max_length=500, blank=True, null=True)
+    session_id = models.CharField(max_length=255, blank=True, null=True)
     money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
